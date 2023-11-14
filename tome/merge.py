3f0635a9cb9bfc6db470279271fb6fd1ce110005
AdityaKane2001 <adityakane1@gmail.com>
2023-11-14 06:50:04 -0800

    added stuff

--- tome/merge.py
+++ tome/merge.py
@@ -15,6 +15,96 @@ def do_nothing(x, mode=None):
     return x
 
 
+def attentive_bipartite_matching(
+    metric: torch.Tensor,
+    attn: torch.Tensor,
+    r: int,
+    class_token: bool = False,
+    distill_token: bool = False,
+) -> Tuple[Callable, Callable]:
+    protected = 0
+    if class_token:
+        protected += 1
+    if distill_token:
+        protected += 1
+
+    # We can only reduce by a maximum of 50% tokens
+    t = metric.shape[1]
+    r = min(r, (t - protected) // 2)
+
+    if r <= 0:
+        return do_nothing, do_nothing
+
+    with torch.no_grad():
+        metric = metric / metric.norm(dim=-1, keepdim=True)
+        
+        # print(f"{attn.shape=}")
+        attn = attn.mean(dim=1) # mean across all heads
+        # print(f"{attn.shape=}")
+        attn = attn.sum(dim=-2) # sum across keys dimensions
+        # print(f"{attn.shape=}")
+        # print(attn)
+        
+        token_importance = attn.argsort(dim=-1)
+        
+        a = metric.gather(dim=-2, index=token_importance.unsqueeze(dim=2).repeat(1, 1, metric.shape[-1])[:, :r, :])
+        b = metric.gather(dim=-2, index=token_importance.unsqueeze(dim=2).repeat(1, 1, metric.shape[-1])[:, r:, :])
+        
+        # print(f"{a.shape=}")
+        # print(f"{b.shape=}")
+        # a, b = metric[..., ::2, :], metric[..., 1::2, :]
+        scores = a @ b.transpose(-1, -2)
+
+        if class_token:
+            scores[..., 0, :] = -math.inf
+        if distill_token:
+            scores[..., :, 0] = -math.inf
+
+        node_max, node_idx = scores.max(dim=-1)
+        edge_idx = node_max.argsort(dim=-1, descending=True)[..., None]
+
+        
+        unm_idx = edge_idx[..., r:, :]  # Unmerged Tokens
+        src_idx = edge_idx[..., :r, :]  # Merged Tokens
+        dst_idx = node_idx[..., None].gather(dim=-2, index=src_idx)
+        # print(f"{unm_idx.shape=}")
+        # print(f"{src_idx.shape=}")
+
+        if class_token:
+            # Sort to ensure the class token is at the start
+            unm_idx = unm_idx.sort(dim=1)[0]
+
+    def merge(x: torch.Tensor, mode="mean") -> torch.Tensor:
+        # print(f"{x.shape=}")
+        # print(f"{token_importance.shape=}")
+        src, dst = x.gather(dim=-2, index=token_importance.unsqueeze(dim=2).repeat(1, 1, x.shape[-1])[:, :r, :]), x.gather(dim=-2, index=token_importance.unsqueeze(dim=2).repeat(1, 1, x.shape[-1])[:, r:, :])
+        n, t1, c = src.shape
+        unm = src.gather(dim=-2, index=unm_idx.expand(n, unm_idx.shape[-2], c))
+        src = src.gather(dim=-2, index=src_idx.expand(n, r, c))
+        dst = dst.scatter_reduce(-2, dst_idx.expand(n, r, c), src, reduce=mode)
+
+        if distill_token:
+            return torch.cat([unm[:, :1], dst[:, :1], unm[:, 1:], dst[:, 1:]], dim=1)
+        else:
+            return torch.cat([unm, dst], dim=1)
+
+    def unmerge(x: torch.Tensor) -> torch.Tensor:
+        unm_len = unm_idx.shape[1]
+        unm, dst = x[..., :unm_len, :], x[..., unm_len:, :]
+        n, _, c = unm.shape
+
+        src = dst.gather(dim=-2, index=dst_idx.expand(n, r, c))
+
+        out = torch.zeros(n, metric.shape[1], c, device=x.device, dtype=x.dtype)
+
+        out[..., 1::2, :] = dst
+        out.scatter_(dim=-2, index=(2 * unm_idx).expand(n, unm_len, c), src=unm)
+        out.scatter_(dim=-2, index=(2 * src_idx).expand(n, r, c), src=src)
+
+        return out
+
+    return merge, unmerge
+
 def bipartite_soft_matching(
     metric: torch.Tensor,
     r: int,
@@ -58,7 +148,7 @@ def bipartite_soft_matching(
 
         node_max, node_idx = scores.max(dim=-1)
         edge_idx = node_max.argsort(dim=-1, descending=True)[..., None]
-
+        
         unm_idx = edge_idx[..., r:, :]  # Unmerged Tokens
         src_idx = edge_idx[..., :r, :]  # Merged Tokens
         dst_idx = node_idx[..., None].gather(dim=-2, index=src_idx)
